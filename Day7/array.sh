35e849c6bbc3f141cf580ea741800738a0c405ff
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-01-20 12:43:19 +0530

    Add: Day7 practice problem added

---
+++ Day7/array.sh
@@ -0,0 +1,8 @@
+counter=0;
+
+Fruits[((counter++))]="Apple"
+Fruits[((counter++))]="Banana"
+Fruits[((counter++))]="Orange"
+Fruits[((counter++))]="Kiwi"
+Fruits[((counter++))]="Papaya"
+echo ${Fruits[@]}
